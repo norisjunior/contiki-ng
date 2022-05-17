67a71aaa8bbcb6ea1e629a5d8d59342e79d3937a
Ferraz Junior, N <norisjunior@gmail.com>
2022-05-17 11:12:40 -0300

    make app with ml_and_msg + cc1352p1

--- lwiotms/lwpubsub/make_app.sh
+++ lwiotms/lwpubsub/make_app.sh
@@ -16,7 +16,11 @@ fi
 make distclean
 
 if [ "$1" == "SENSORTAG" ] ; then
-  make lwpubsub-fedsensor-lwaiot TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=1 lwpubsub-fedsensor-lwaiot
+  make lwpubsub-fedsensor-ml_and_msg TARGET=cc26x0-cc13x0 BOARD=sensortag/cc2650 MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=1 lwpubsub-fedsensor-ml_and_msg
 elif [ "$1" == "REMOTE" ] ; then
-  make lwpubsub-fedsensor-lwaiot TARGET=zoul BOARD=remote-revb PORT=/dev/ttyUSB1 MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=1 lwpubsub-fedsensor-lwaiot.upload
+  make lwpubsub-fedsensor-ml_and_msg TARGET=zoul BOARD=remote-revb PORT=/dev/ttyUSB1 MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=1 lwpubsub-fedsensor-ml_and_msg.upload
+elif [ "$1" == "NATIVE" ] ; then
+  make lwpubsub-fedsensor-ml_and_msg TARGET=native MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=0 lwpubsub-fedsensor-ml_and_msg
+elif [ "$1" == "CC1352P1" ] ; then
+  make lwpubsub-fedsensor-ml_and_msg TARGET=simplelink BOARD=launchpad/cc1352p1 MAKE_KEYSIZE=128 MAKE_CRYPTOMODE=CCM MAKE_WITH_ENERGY=1 lwpubsub-fedsensor-ml_and_msg
 fi
