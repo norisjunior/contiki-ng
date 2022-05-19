227dc5f09650e77125b2e31fa75469f84a8a59a5
Ferraz Junior, N <norisjunior@gmail.com>
2022-05-19 11:26:09 -0300

    display centroids info on INFO log not only DBG

--- lwiotms/lwpubsub/lwpubsub-fedsensor-ml_and_msg.c
+++ lwiotms/lwpubsub/lwpubsub-fedsensor-ml_and_msg.c
@@ -2286,6 +2286,7 @@ pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk,
           // não fazer predict aqui, esperar o tempo
 
           //kmeans_predict(); //K-means LWAIoT message
+          LOG_INFO("Centroids: ");
 
           line = 0;
           column = 0;
@@ -2301,7 +2302,7 @@ pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk,
 
             if ( (commandReceived[i] == semic[0]) ) { //pula pra próxima linha
               centroids[line][column] = strtof(valuereceived, NULL);
-              LOG_DBG_(" - centroids[%d][%d]: %.2f\n", line, column, centroids[line][column]);
+              LOG_INFO_(" - centroids[%d][%d]: %.2f\n", line, column, centroids[line][column]);
               line++;
               column = 0;
               m = 0;
@@ -2311,7 +2312,7 @@ pub_handler(const char *topic, uint16_t topic_len, const uint8_t *chunk,
 
             if ( (commandReceived[i] == hashtag[0]) || (i == commandReceivedlen-1) ) { //pula pra próxima coluna
               centroids[line][column] = strtof(valuereceived, NULL);
-              LOG_DBG_(" - centroids[%d][%d]: %.2f", line, column, centroids[line][column]);
+              LOG_INFO_(" - centroids[%d][%d]: %.2f", line, column, centroids[line][column]);
               column++;
               m = 0;
               memset(valuereceived, 0, 10);
